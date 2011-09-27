780a022ff0fb6a5d2c741668399f92dd893647af
Denis Bazhan <denis.bazhan@gmail.com>
2011-09-27 06:54:13 +0300

    User model with password, and user show page

--- Gemfile
+++ Gemfile
@@ -1,11 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.1.0'
-
-# Bundle edge Rails instead:
-# gem 'rails',     :git => 'git://github.com/rails/rails.git'
-
 gem 'sqlite3'
+gem 'gravatar_image_tag'
 
 group :development do
   gem 'rspec-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,52 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
-
+require "digest"
 class User < ActiveRecord::Base
-  attr_accessible :name, :email #Доступные атрибуты в целях безопасности
+  attr_accessor :password
 
-  validates :name, :presence => true
+  #Доступные атрибуты в целях безопасности
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  #Валидация
+  validates :name, :presence => true,
+                   :length   => { :maximum => 50 }
+
+  validates :email, :presence => true
+
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  #self.authenticate это User.authenticate так удобнее
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypt_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
 
