73a2d26f6982afa988ec49c1a06b03f220b9aecf
Denis Bazhan <denis.bazhan@gmail.com>
2011-09-27 04:49:48 +0300

    Finished first cut of the User model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :name, :email #Доступные атрибуты в целях безопасности
+
+  validates :name, :presence => true
+end
+
